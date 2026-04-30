18dc8ce693995fe9846d92ff5366302522695252
Drita-ai <alok.kr300@gmail.com>
2026-04-30 23:30:07 +0530

    feat: display all files of specified dir

--- src/server/src/core/tools/file_explorer.py
+++ src/server/src/core/tools/file_explorer.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 from directory_tree import DisplayTree
 
 
@@ -8,3 +10,24 @@ def get_directory_tree(ROOT_DIR) -> str | None:
     Returns tree representation of provided Directory
     """
     return DisplayTree(ROOT_DIR, ignoreList=EXCLUDED)
+
+def _get_all_files(ROOT_DIR):
+    """
+    Returns  all files of from the provided directory
+    """
+    _root_dir = Path(ROOT_DIR)
+    
+    _excluded_suffixes = tuple(EXCLUDED)
+    
+    for item in _root_dir.iterdir():
+        if item.name in EXCLUDED or item.name.startswith('.'):
+            continue
+        
+        if item.name.endswith(_excluded_suffixes):
+            continue
+        
+        if item.is_file():
+            yield item
+            
+        if item.is_dir():
+            yield from _get_all_files(item)
